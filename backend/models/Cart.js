fb67491e4d4a285bbd0705dfc0c5bb637cbebe12
dilipkumars067 <dilipkumarsdk205@gmail.com>
2026-06-29 10:27:09 +0530

    Add Cart model

---
+++ backend/models/Cart.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Cart = sequelize.define('Cart', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1
+  },
+  selectedSize: {
+    type: DataTypes.STRING
+  },
+  selectedColor: {
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Cart;
