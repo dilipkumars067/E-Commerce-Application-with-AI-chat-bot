7041e297babc6c693c3db14b376191bf9e9b94b1
dilipkumars067 <dilipkumarsdk205@gmail.com>
2026-06-29 10:27:50 +0530

    Add Review model

---
+++ backend/models/Review.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Review = sequelize.define('Review', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: { min: 1, max: 5 }
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  comment: {
+    type: DataTypes.TEXT
+  },
+  isVerifiedPurchase: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  helpful: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Review;
