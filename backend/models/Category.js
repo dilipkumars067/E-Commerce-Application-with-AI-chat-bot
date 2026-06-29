0056770950a02d68d925870f1f1ba5c0b8b4d277
dilipkumars067 <dilipkumarsdk205@gmail.com>
2026-06-29 10:26:56 +0530

    Add Category model

---
+++ backend/models/Category.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  image: {
+    type: DataTypes.STRING
+  },
+  slug: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Category;
